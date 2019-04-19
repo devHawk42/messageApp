74c30652cdbb23792d38180ee64f9005b8a554f0
devHawk42 <devhawk42@gmail.com>
2019-04-19 11:50:16 -0300

    setup components/file structure

---
+++ messageApp/src/components/screens/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+
+class Home extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    HI! react native app.
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default Home;
---
+++ messageApp/src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+    turboAppId: '5cb8ceb61d73fd001544b664',
+
+};
